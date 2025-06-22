7b80371b3c74766c7bf771b4b9eff3754d5ea87e
JSW <xquadoxx@gmail.com>
2025-06-22 17:21:33 +0900

    ORPJ-109: [전체 QA] 탈퇴 모달 문구 및 플로우 개선

--- src/pages/service/faq/faq.js
+++ src/pages/service/faq/faq.js
@@ -5,23 +5,9 @@ import RequestSender from '@library/RequestSender';
 
 import { checkAuthorization } from '@library/CommonLib.js';
 import { logout } from '@library/ServiceCommonLib.js';
-import { checkNextStep } from '@library/ServiceCommonLib';
-
 
 window.addEventListener('pageshow', async () => {
     await checkAuthorization('API_KEY');
-    const nextStep = await checkNextStep();
-    switch (nextStep) {
-        case 'onboarding':
-            window.location.href = '/onboarding';
-            return;
-        case 'setting':
-            window.location.href = '/setting';
-            return;
-        case 'collect':
-        default:
-            break;
-    }
 });
 
 const $faqs = document.querySelectorAll('.faq');
@@ -84,6 +70,7 @@ function closeExitModal() {
 
     $exitModalExitReason.value = '';
     $exitModalSubmitBtn.classList.remove('active');
+    byteCounter.innerHTML = '';
 }
 
 /* byte counter 추가  ORPJ-95 > ORPJ-109 */
