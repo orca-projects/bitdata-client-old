a67f37eadaa37af315e83f30ee2434d1d384d907
hurryduck <tigeryck0812@gmail.com>
2025-06-23 20:53:37 +0900

    [ORPJ-123] [전체 QA] 모바일 홈페이지 새로고침 UX 개선

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ let curSectionIndex = 0;
 let isPlayAnimation = false;
 const animationPlayTime = 2000;
 
-let touchStartY = 0;
-
 function scrollEvent(e) {
     stopScroll(e);
     if (!isPlayAnimation) {
@@ -53,26 +51,7 @@ function clickPaginationBtn(index) {
     }
 }
 
-function handleTouchStart(e) {
-    touchStartY = e.touches[0].clientY;
-}
-
-function handleTouchEnd(e) {
-    const touchEndY = e.changedTouches[0].clientY;
-    const deltaY = touchStartY - touchEndY;
-
-    if (!isPlayAnimation) {
-        if (deltaY > 30 && curSectionIndex + 1 < $scrollSectionItems.length) {
-            scrollAnimationStart(() => scrollToIndex(curSectionIndex + 1));
-        } else if (deltaY < -30 && curSectionIndex - 1 >= 0) {
-            scrollAnimationStart(() => scrollToIndex(curSectionIndex - 1));
-        }
-    }
-}
-
 window.addEventListener('wheel', scrollEvent, { passive: false });
-window.addEventListener('touchstart', handleTouchStart, { passive: false });
-window.addEventListener('touchend', handleTouchEnd, { passive: false });
 
 updatePaginationBtn();
 
